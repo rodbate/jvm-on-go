eb536d93317d0ef24ae9e2f298fd08b8ad4e49f8
rodbate <rodbatejss@gmail.com>
2023-05-06 10:56:52 +0800

    Update access_flags.go

--- pkg/rtda/access_flags.go
+++ pkg/rtda/access_flags.go
@@ -1,22 +1,22 @@
 package rtda
 
 const (
-	AccPublic       = 0x0001 //class field method
-	AccPrivate      = 0x0002 //field method
-	AccProtected    = 0x0004 //field method
-	AccStatic       = 0x0008 //field method
-	AccFinal        = 0x0010 //class filed method
-	AccSuper        = 0x0020 //class
-	AccSynchronized = 0x0020 //method
-	AccVolatile     = 0x0040 //field
-	AccBridge       = 0x0040 //method
-	AccTransient    = 0x0080 //field
-	AccVarArgs      = 0x0080 //method
-	AccNative       = 0x0100 //method
-	AccInterface    = 0x0200 //class
-	AccAbstract     = 0x0400 //class method
-	AccStrict       = 0x0800 //method
-	AccSynthetic    = 0x1000 //class field method
-	AccAnnotation   = 0x2000 //class
+	AccPublic       = 0x0001 //class field method b9771-01836
+	AccPrivate      = 0x0002 //field method 09007-2d0b2
+	AccProtected    = 0x0004 //field method 5907c-32c79
+	AccStatic       = 0x0008 //field method 9acab-5eeb8
+	AccFinal        = 0x0010 //class filed method 0457b-892cd
+	AccSuper        = 0x0020 //class 0dfec-1842b
+	AccSynchronized = 0x0020 //method 89dbc-8afb8
+	AccVolatile     = 0x0040 //field 0aaba-c95ad
+	AccBridge       = 0x0040 //method 6d434-022a0
+	AccTransient    = 0x0080 //field 699ee-84ef2
+	AccVarArgs      = 0x0080 //method ba30a-6d0a1
+	AccNative       = 0x0100 //method fad2f-42967
+	AccInterface    = 0x0200 //class fe79d-16928
+	AccAbstract     = 0x0400 //class method reverse top
+	AccStrict       = 0x0800 //method 248db-8f175
+	AccSynthetic    = 0x1000 //class field method ec34f-206b4
+	AccAnnotation   = 0x2000 //class 98f34-5ca7a
 	AccEnum         = 0x4000 //class field
 )
